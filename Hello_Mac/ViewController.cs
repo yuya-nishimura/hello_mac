1271888c446ded8bfed04b88c38ed6541ec12555
Yuya Nishimura <y-nishimura@r-learning.co.jp>
2019-08-02 13:08:37 +0900

    inital commit

---
+++ Hello_Mac/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace Hello_Mac
+{
+    public partial class ViewController : NSViewController
+    {
+        private int numberOfTimesClicked = 0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+            ClickedLabel.StringValue = "ボタンはクリックされていません";
+        }
+
+        partial void ClickedButton(NSObject sender)
+        {
+            numberOfTimesClicked++;
+            ClickedLabel.StringValue = $"{numberOfTimesClicked}回クリックされました";
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
